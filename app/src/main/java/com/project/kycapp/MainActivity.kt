8a6cc585bf2a9ee7cdb8354e039d8774f0654ac4
Tanaka Ian Kahwai <iankahwai@gmail.com>
2022-01-18 12:09:05 +0200

    Initialize project

---
+++ app/src/main/java/com/project/kycapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.project.kycapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.project.kycapp.ui.theme.KycAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KycAppTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    Text("Hello")
+                }
+            }
+        }
+    }
+}
+
